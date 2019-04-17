3cc7997598e78d493330bc0ad194ad243b82711a
tommasocasucci <tommaso.casucci@gmail.com>
2019-04-17 13:24:21 +0100

    zTool new Space Colonization and Wave Function Collapse classes added

---
+++ zTools/zTools/WaveFunctionCollapse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zTools.zTools
+{
+    public class WaveFunctionCollapse
+    {
+        //TO DO import from previous version of Zcode
+
+    }
+}
