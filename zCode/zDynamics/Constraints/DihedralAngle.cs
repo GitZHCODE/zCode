ecc72a113ea053257a076c5e74cdbf3a4b8d354b
tommasocasucci <tommaso.casucci@gmail.com>
2020-02-13 19:45:36 +0000

    Updated Dihedral Angle IConstraint

--- zCode/zDynamics/Constraints/DihedralAngle.cs
+++ zCode/zDynamics/Constraints/DihedralAngle.cs
@@ -16,12 +16,27 @@ namespace zCode.zDynamics
     [Serializable]
     public class DihedralAngle : Constraint, IConstraint
     {
+        #region Static Members
+
+        /// <summary>
+        /// Assumes both angles are between 0 and 2PI
+        /// </summary>
+        /// <param name="a0"></param>
+        /// <param name="a1"></param>
+        /// <returns></returns>
+        private static double GetMinAngleDifference(double a0, double a1)
+        {
+            var d0 = (a0 < a1) ? a0 - a1 + 2.0 * Math.PI : a0 - a1;
+            return d0 > Math.PI ? d0 - 2.0 * Math.PI : d0;
+        }
+
+        #endregion
+
         private H _h0 = new H();
         private H _h1 = new H();
         private H _h2 = new H();
         private H _h3 = new H();
         private double _target;
-
 
         /// <summary>
         /// 
@@ -39,7 +54,7 @@ namespace zCode.zDynamics
             _h2.Index = left;
             _h3.Index = right;
 
-            Target = target;
+            _target = target;
             Weight = weight;
         }
 
@@ -81,12 +96,11 @@ namespace zCode.zDynamics
 
 
         /// <summary>
-        /// Note this value is wrapped between 0 and 2PI.
+        /// 
         /// </summary>
         public double Target
         {
             get { return _target; }
-            set { _target = zMath.Mod(value, zMath.TwoPI); }
         }
 
 
@@ -100,45 +114,42 @@ namespace zCode.zDynamics
         /// <inheritdoc />
         public void Calculate(IReadOnlyList<IBody> bodies)
         {
-            // TOOD
-            // test implementation
-
-            Vec3d p0 = bodies[_h0].Position;
-            Vec3d p1 = bodies[_h1].Position;
-            Vec3d p2 = bodies[_h2].Position;
-            Vec3d p3 = bodies[_h3].Position;
+            
+            //Method 01
+            Vec3d p0 = bodies[_h0.Index].Position;
+            Vec3d p1 = bodies[_h1.Index].Position;
+            Vec3d p2 = bodies[_h2.Index].Position;
+            Vec3d p3 = bodies[_h3.Index].Position;
 
             var rotation = AxisAngle3d.Identity;
             rotation.Axis = p1 - p0;
 
-            var angle = GeometryUtil.GetDihedralAngle(rotation.Axis, Vec3d.Cross(rotation.Axis, p2 - p1), Vec3d.Cross(rotation.Axis, p0 - p3));
-            rotation.Angle = (_target - angle) * 0.5;
+            var d2 = p2 - p0;
+            var d3 = p3 - p0;
 
-            /*
-            // calculate deltas
-            _h3.Delta = rotation.Apply(p3) - p3;
-            rotation.Invert();
-            _h2.Delta = rotation.Apply(p2) - p2;
-            */
+            var angle = GeometryUtil.GetDihedralAngle(rotation.Axis, Vec3d.Cross(rotation.Axis, d2), Vec3d.Cross(rotation.Axis, -d3)) + Math.PI;
+            rotation.Angle = GetMinAngleDifference(_target, angle) * 0.5;
 
-            // calc deltas
-            var d3 = rotation.Apply(p3) - p3;
-            rotation.Invert();
-            var d2 = rotation.Apply(p2) - p2;
+            
 
-            _h0.Delta = _h1.Delta = (d2 + d3) * 0.25; // distribute projection bw pair
-            _h2.Delta = d2;
-            _h3.Delta = d3;
+            // calculate deltas as diff bw current and rotated
+            _h2.Delta = (rotation.Inverse.Apply(d2) - d2) * 0.5;
+            _h3.Delta = (rotation.Apply(d3) - d3) * 0.5;
+
+            // distribute reverse projection among hinge bodies
+            _h0.Delta = _h1.Delta = (_h2.Delta + _h3.Delta) * -0.5;
+            
+
         }
 
 
         /// <inheritdoc />
         public void Apply(IReadOnlyList<IBody> bodies)
         {
-            bodies[_h0].ApplyMove(_h0.Delta, Weight);
-            bodies[_h1].ApplyMove(_h1.Delta, Weight);
-            bodies[_h2].ApplyMove(_h2.Delta, Weight);
-            bodies[_h3].ApplyMove(_h3.Delta, Weight);
+            bodies[_h0.Index].ApplyMove(_h0.Delta, Weight);
+            bodies[_h1.Index].ApplyMove(_h1.Delta, Weight);
+            bodies[_h2.Index].ApplyMove(_h2.Delta, Weight);
+            bodies[_h3.Index].ApplyMove(_h3.Delta, Weight);
         }
 
 
